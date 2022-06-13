a501d51241e070a8ff2f137f6dbc4cf30f68621a
Abdulqadir51 <abdulessa93@gmail.com>
2022-06-14 02:07:55 +0500

    Inherit classes and methods and generated markup dynamically

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
 var fs = require('fs');
-var generateHTML = require('./utils/generateMarkdown');
+var generateHTML = require('./utils/generateMarkup');
+var Employee = require('./lib/Employee');
+var Manager = require('./lib/Manager');
+var Engineer = require('./lib/Engineer');
+var Intern = require('./lib/Intern');
 
 
+// My team object for storing team members
+myTeam = [];
 
-// TODO: Create an array of questions for user input
-const questions = [
-
+// Manager questions
+const q_manager = [
+    { name: 'name', message: 'Please enter the team manager’s name' },
+    { name: 'Id', message: 'Please enter the manager’s Id' },
+    { name: 'email', message: 'Please enter the email address' },
+    { name: 'officeNumber', message: 'Please enter the office number' },
 ];
+
+// Engineer questions
+const q_engineer = [
+    { name: 'name', message: 'Please enter the Engineer’s name' },
+    { name: 'Id', message: 'Please enter the Engineer’s Id' },
+    { name: 'email', message: 'Please enter the email address' },
+    { name: 'githubUsername', message: 'Please enter the Github username' },
+]
+
+// Intern questions
+const q_intern = [
+    { name: 'name', message: 'Please enter the Intern’s name' },
+    { name: 'Id', message: 'Please enter the Intern’s Id' },
+    { name: 'email', message: 'Please enter the email address' },
+    { name: 'school', message: 'Please enter the school name' },
+]
+
+// Add new Team members
+const q_add_team_members = [{
+    name: 'member',
+    message: 'Do you you want to add an engineer or an intern or to finish building the team?',
+    type: 'list',
+    choices: [
+        'Engineer', 'Intern', 'Finish building team'
+    ],
+}]
+
 
 
 // TODO: Create a function to write HTML file
@@ -18,11 +54,17 @@ function writeToFile(fileName, data) {
     });
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        writeToFile('./dist/index.html', answers);
-        console.log("HTML File Generated Successfully !");
+promptNewMembers = () => {
+    inquirer.prompt(q_add_team_members).then((answers) => {
+        if (answers['member'] == "Finish building team") {
+            finishBuilding();
+
+        } else if (answers['member'] == "Engineer") {
+            promptEngineer();
+        } else {
+            promptIntern();
+        }
+
 
     }).catch((error) => {
         if (error.isTtyError) {
@@ -33,7 +75,70 @@ function init() {
             console.log(error)
         }
     });
+}
 
+promptManager = () => {
+    inquirer.prompt(q_manager).then((a) => {
+        var m = new Manager(a.name, a.Id, a.email, a.officeNumber);
+        myTeam.push(m);
+        promptNewMembers();
+
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            console.log(error.message)
+        } else {
+            // Something else went wrong
+            console.log(error)
+        }
+    });
+}
+
+promptEngineer = () => {
+    inquirer.prompt(q_engineer).then((a) => {
+        var e = new Engineer(a.name, a.Id, a.email, a.githubUsername);
+        myTeam.push(e);
+        promptNewMembers();
+
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            console.log(error.message)
+        } else {
+            // Something else went wrong
+            console.log(error)
+        }
+    });
+}
+
+
+promptIntern = () => {
+    inquirer.prompt(q_intern).then((a) => {
+
+        var i = new Intern(a.name, a.Id, a.email, a.school);
+        myTeam.push(i);
+        promptNewMembers();
+
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            console.log(error.message)
+        } else {
+            // Something else went wrong
+            console.log(error)
+        }
+    });
+}
+
+finishBuilding = () => {
+    writeToFile('./dist/index.html', myTeam);
+    console.log("HTML File Generated Successfully !");
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+    promptManager();
 }
 
 // Function call to initialize app
--- utils/generateMarkup.js
+++ utils/generateMarkup.js
@@ -1,7 +1,106 @@
 // To generate HTML from the data
-function generateHTML(data) {
+function generateHTML(members) {
 
-    var markup = ``
+    var markup = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Team Profile</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
+        <link href="style.css" rel="stylesheet" type="text/css" />
+      </head>
+      <body>
+        <nav class="navbar bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand">My Team</a>
+          </div>
+        </nav>
+        <div class="content">
+          <div class="container">
+            <div class="row">
+             `
+
+    // Team Members
+    for (let i = 0; i < members.length; i++) {
+
+        // Team Members Name and Role
+        markup += `<div class="col-lg-4">
+        <div class="text-center card-box">
+          <div class="member-card pt-2 pb-2">
+            <div class="thumb-lg member-thumb mx-auto">
+              <div class="rounded-circle img-thumbnail">
+                <i class="fa fa-user"></i>
+              </div>
+            </div>
+            <div class="">
+              <h4>${members[i].getName()}</h4>
+              <p class="text-muted">
+                @${members[i].getRole()} 
+              </p>
+            </div>`
+
+
+        // Engineers github Username
+        if (members[i].getRole() == 'Engineer') {
+
+            markup += ` <ul class="social-links list-inline">
+                <li class="list-inline-item">
+                  <a
+                    title=""
+                    data-placement="top"
+                    data-toggle="tooltip"
+                    class="tooltips"
+                    href="https://github.com/${members[i].getGithub()}"
+                    data-original-title="Github"><i class="fa fa-github"></i></a>
+                </li>
+              </ul>`
+        }
+
+        // Manager office number
+        if (members[i].getRole() == 'Manager') {
+
+            markup += ` <ul class="social-links list-inline">
+              <li class="list-inline-item">
+               <p>Office Number: ${members[i].getOfficeNumber()}</p>
+              </li>
+            </ul>`
+        }
+        // Intern School
+        if (members[i].getRole() == 'Intern') {
+
+            markup += ` <ul class="social-links list-inline">
+            <li class="list-inline-item">
+             <p>School: ${members[i].getSchool()}</p>
+            </li>
+          </ul>`
+        }
+
+        // Team Members Email
+        markup += `<a href="mailto:${members[i].getEmail()}"
+              class="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">
+              Email Now
+            </a>
+          </div>
+        </div>
+      </div>`
+    }
+
+    markup += `</div>
+    </div>
+    <!-- container -->
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+    </html>`
+    return markup
 }
 
 module.exports = generateHTML
